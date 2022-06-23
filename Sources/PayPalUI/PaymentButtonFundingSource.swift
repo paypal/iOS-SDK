947f24adac7efd73077bfa7b1653a0d333e469fb
Minh Nguyen <thenguyen@paypal.com>
2022-06-23 22:40:21 +0700

    Remove PaymentButton

--- Sources/PayPalUI/PaymentButtonFundingSource.swift
+++ Sources/PayPalUI/PaymentButtonFundingSource.swift
@@ -1,8 +1,1 @@
 import Foundation
-
-/// The funding source to be used when checkout with PaymentButton
-public enum PaymentButtonFundingSource: String {
-    case payPal = "PayPal"
-    case payLater = "Pay Later"
-    case credit = "Credit"
-}
